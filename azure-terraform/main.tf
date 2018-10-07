21f165f48c7743ec0a252be683c793daee310796
Jamie Phillips <cphill11@gmail.com>
2018-10-06 21:02:12 -0400

    Formatting the terraform.

--- azure-terraform/main.tf
+++ azure-terraform/main.tf
@@ -1,16 +1,16 @@
 # Configure the Azure Provider
 provider "azurerm" {
-    version = "~> 1.16"
- }
+  version = "~> 1.16"
+}
 
 # Tags for reuse
 locals {
-    tags = {
-        CostCenter = "Demo"
-        Contact = "Jamie"
-        Managed = "Terraformed"
-        Environment = "Dev"
-    }
+  tags = {
+    CostCenter  = "Demo"
+    Contact     = "Jamie"
+    Managed     = "Terraformed"
+    Environment = "Dev"
+  }
 }
 
 # Create a resource group
@@ -18,9 +18,8 @@ resource "azurerm_resource_group" "myApp" {
   name     = "MyApp-Demo-RG"
   location = "${var.region}"
 
-  tags ="${local.tags}"
+  tags = "${local.tags}"
 }
-
 
 resource "azurerm_app_service_plan" "myApp" {
   name                = "MyApp-Demo-AP"
@@ -32,7 +31,7 @@ resource "azurerm_app_service_plan" "myApp" {
     size = "F1"
   }
 
-  tags ="${local.tags}"
+  tags = "${local.tags}"
 }
 
 resource "azurerm_app_service" "myApp" {
@@ -40,7 +39,7 @@ resource "azurerm_app_service" "myApp" {
   location            = "${azurerm_resource_group.myApp.location}"
   resource_group_name = "${azurerm_resource_group.myApp.name}"
   app_service_plan_id = "${azurerm_app_service_plan.myApp.id}"
-  https_only = true
+  https_only          = true
 
-  tags ="${local.tags}"
+  tags = "${local.tags}"
 }
--- azure-terraform/variables.tf
+++ azure-terraform/variables.tf
@@ -1,3 +1,3 @@
 variable "region" {
-  default= "East US"
+  default = "East US"
 }
